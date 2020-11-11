626c8f196ce7c9d3d7ddd351eaf4689437fcc842
Ruslan Gainutdinov <contact@ruslan.org>
2020-11-12 00:42:34 +0200

    Fix login

--- build-macos.sh
+++ build-macos.sh
@@ -9,5 +9,6 @@ npx nativefier --name "Google Calendar" \
     --icon google-calendar.png \
     --counter \
     --bounce \
+    --user-agent "Mozilla/5.0 (Windows NT 10.0; rv:74.0) Gecko/20100101 Firefox/74.0" \
     --internal-urls "(calendar\.google\.*?)|(calendar\.ruslan\.org)|(localhost:3000)|(accounts\.google\.com)" \
     https://calendar.google.com
